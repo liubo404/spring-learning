b06be80a53760703aa48039f4b675c43327b82b3
liubo <1091643978@qq.com>
2020-11-22 19:51:45 +0800

    slection

---
+++ data-alog/src/main/java/com/lbo/alogs/sorts/SelectionSort.java
@@ -0,0 +1,8 @@
+package com.lbo.alogs.sorts;
+
+/**
+ * @author liubo
+ * @date 2020/11/22
+ */
+public class SelectionSort {
+}
---
+++ data-alog/src/test/java/com/lbo/alogs/sorts/SelectionSortTest.java
@@ -0,0 +1,29 @@
+package com.lbo.alogs.sorts;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author liubo
+ * @date 2020/11/22
+ */
+@RunWith(Arquillian.class)
+public class SelectionSortTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(SelectionSort.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    public void selectMinumSort() {
+    }
+}
